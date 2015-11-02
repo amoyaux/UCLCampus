357b1d9f2b93134b0eb41c0b8f3d8a1b6e381335
amoyaux <arnold.moyaux@student.uclouvain.be>
2015-11-02 11:57:19 +0100

    new style

--- UCLCampus/www/js/app.js
+++ UCLCampus/www/js/app.js
@@ -1,15 +1,33 @@
-angular.module('starter', ['ionic'])
+angular.module('ionicApp', ['ionic'])
 
-.controller('TodoCtrl', function($scope, $ionicModal) {
+.config(function($stateProvider, $urlRouterProvider) {
 
+  $stateProvider
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "app.html"
+    })
+    .state('app.home', {
+      url: "/home",
+      views: {
+        'appContent' :{
+          templateUrl: "home.html",
+          controller : "HomeController"
+        }
+      }
+    })
+  
+  $urlRouterProvider.otherwise("/app/home");
+})
 
-  // Create our modal
-  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
-    $scope.taskModal = modal;
-  }, {
-    scope: $scope
-  });
+.controller('AppController', function($scope, $ionicSideMenuDelegate) {
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+})
 
+.controller("HomeController", function($scope) {
   $scope.newTask = function() {
     $scope.taskModal.show();
   };
@@ -20,6 +38,33 @@ angular.module('starter', ['ionic'])
   $scope.slide = function(index) {
     $ionicSlideBoxDelegate.slide(index);
   };
+})
 
-});
+.controller("CartController", function($scope) {
+  
+  $scope.data = {
+    items : []
+  };
+  
+  for(var i = 0; i < 25; i++) {
+    $scope.data.items.push({
+      id : i,
+      label : "Item " + i
+    })
+  }
+  
+})
 
+.directive("ionCart", function() {
+  return {
+    restrict : "E",
+    templateUrl : "ionCart.html"
+  }
+})
+
+.directive("ionPurchase", function() {
+  return {
+    restrict : "E",
+    template : "<h2>This is Ion Purchase</h2>"
+  }
+})
