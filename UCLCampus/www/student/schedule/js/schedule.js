efc83137ba6ea0652fa99483654fe9ec25a71a8a
amoyaux <arnold.moyaux@student.uclouvain.be>
2016-03-04 13:12:09 +0100

    schedule debug

--- UCLCampus/www/student/schedule/js/schedule.js
+++ UCLCampus/www/student/schedule/js/schedule.js
@@ -35,7 +35,9 @@ $scope.toggleLeft= function(){
 	tomorrow.setDate($scope.datepickerObject.inputDate.getDate()+1);
 	$scope.datepickerObject.inputDate = tomorrow;
 }
-
+$scope.refresh= function(){
+	$scope.refresh = 0;
+}
 
 $scope.$on('$ionicView.enter', function() {
 	function weeks(){
@@ -46,134 +48,140 @@ $scope.$on('$ionicView.enter', function() {
 		res = res + 54;
 		return res;
 	}
+	var schedList = window.localStorage['schedule'] || 'fail';
+	if (schedList = 'fail'){
+		$http({
+		  method: 'GET',
+		  url: 'http://horairev6.uclouvain.be/jsp/custom/modules/plannings/direct_planning.jsp?weeks=' + weeks() + '&code='+'lingi2347,lingi2262'+'&login=etudiant&password=student&projectId=12&showTabDuration=true&showTabDate=true&showTab=true&showTabWeek=false&showTabDay=false&showTabStage=false&showTabResources=false&showTabCategory6=false&showTabCategory7=false&showTabCategory8=false'
+		  }).then(function successCallback(response) {
+		    $http({
+		  		method: 'GET',
+		  		url: 'http://horairev6.uclouvain.be/jsp/custom/modules/plannings/info.jsp?displayConfName=WEB&order=slot'
+		  		}).then(function successCallback(response) {
+		  			function skip(table){
+		  				a = table.indexOf('</td>');
+		  				b = table.lastIndexOf('</td>');
+		  				table = table.substring(a+5);
+		  				return table;
+		  			}
 
-	$http({
-	  method: 'GET',
-	  url: 'http://horairev6.uclouvain.be/jsp/custom/modules/plannings/direct_planning.jsp?weeks=' + weeks() + '&code='+'lingi2347,lingi2262'+'&login=etudiant&password=student&projectId=12&showTabDuration=true&showTabDate=true&showTab=true&showTabWeek=false&showTabDay=false&showTabStage=false&showTabResources=false&showTabCategory6=false&showTabCategory7=false&showTabCategory8=false'
-	  }).then(function successCallback(response) {
-	    $http({
-	  		method: 'GET',
-	  		url: 'http://horairev6.uclouvain.be/jsp/custom/modules/plannings/info.jsp?displayConfName=WEB&order=slot'
-	  		}).then(function successCallback(response) {
-	  			function skip(table){
-	  				a = table.indexOf('</td>');
-	  				b = table.lastIndexOf('</td>');
-	  				table = table.substring(a+5);
-	  				return table;
-	  			}
-	  			console.log(response);
+		  			var doc = response.data;
+		  			var a = doc.indexOf('<table>');
+		  			var b = doc.indexOf('</table>');
+		  			var doc = doc.substring(a+7,b);
+		  			for(i = 0; i <2 ; i++){
+		  				var a = doc.indexOf('</tr>');
+		  				var doc = doc.substring(a+5,b);
+		  			}
+		  			var a = doc.indexOf('<tr');
 
-	  			var doc = response.data;
-	  			var a = doc.indexOf('<table>');
-	  			var b = doc.indexOf('</table>');
-	  			var doc = doc.substring(a+7,b);
-	  			for(i = 0; i <2 ; i++){
-	  				var a = doc.indexOf('</tr>');
-	  				var doc = doc.substring(a+5,b);
-	  			}
-	  			var a = doc.indexOf('<tr');
+		  			var list_schedule = [];
+		  			while(a != -1){
+		  				//frame of the schedule for one lecture
+		  				b = doc.indexOf('</tr>');
+		  				var table = doc.substring(a+4,b);
 
-	  			var list_schedule = [];
-	  			while(a != -1){
-	  				//frame of the schedule for one lecture
-	  				b = doc.indexOf('</tr>');
-	  				var table = doc.substring(a+4,b);
+		  				//Date
+		  				a = table.indexOf('<td');
+		  				b = table.indexOf('</td>');
+		  				var frame = table.substring(a+4,b);
+		  				a = frame.indexOf('>');
+		  				b = frame.indexOf('</');
+		  				frame = frame.substring(a+1,b);
 
-	  				//Date
-	  				a = table.indexOf('<td');
-	  				b = table.indexOf('</td>');
-	  				var frame = table.substring(a+4,b);
-	  				a = frame.indexOf('>');
-	  				b = frame.indexOf('</');
-	  				frame = frame.substring(a+1,b);
+		  				var date = frame;
+		  				var s = date.split('/');
+		  				date = s[1] + "/" + s[0] + "/" + s[2];
 
-	  				var date = frame;
-	  				var s = date.split('/');
-	  				date = s[1] + "/" + s[0] + "/" + s[2];
+		  				//Remove frame from table
+		  				table = skip(table);
 
-	  				//Remove frame from table
-	  				table = skip(table);
+		  				//Skip
+		  				table = skip(table);
+		  				
+		  				//StartDate
+		  				a = table.indexOf('<td');
+		  				b = table.indexOf('</td>');
+		  				var frame = table.substring(a+4,b);
 
-	  				//Skip
-	  				table = skip(table);
-	  				
-	  				//StartDate
-	  				a = table.indexOf('<td');
-	  				b = table.indexOf('</td>');
-	  				var frame = table.substring(a+4,b);
+		  				var startDate = frame;
+		  				table = skip(table);
 
-	  				var startDate = frame;
-	  				table = skip(table);
+		  				//Duration
+		  				a = table.indexOf('<td');
+		  				b = table.indexOf('</td>');
+		  				var frame = table.substring(a+4,b);
 
-	  				//Duration
-	  				a = table.indexOf('<td');
-	  				b = table.indexOf('</td>');
-	  				var frame = table.substring(a+4,b);
+		  				var duration = frame;
+		  				table = skip(table);
 
-	  				var duration = frame;
-	  				table = skip(table);
+		  				//Skip
+		  				table = skip(table);
+		  				table = skip(table);
+		  				table = skip(table);
 
-	  				//Skip
-	  				table = skip(table);
-	  				table = skip(table);
-	  				table = skip(table);
+		  				//Professor
+		  				a = table.indexOf('<td');
+		  				b = table.indexOf('</td>');
+		  				var frame = table.substring(a+4,b);
 
-	  				//Professor
-	  				a = table.indexOf('<td');
-	  				b = table.indexOf('</td>');
-	  				var frame = table.substring(a+4,b);
+		  				var professor = frame;
+		  				table = skip(table);
 
-	  				var professor = frame;
-	  				table = skip(table);
+		  				//Local
+		  				a = table.indexOf('<td');
+		  				b = table.indexOf('</td>');
+		  				var frame = table.substring(a+4,b);
 
-	  				//Local
-	  				a = table.indexOf('<td');
-	  				b = table.indexOf('</td>');
-	  				var frame = table.substring(a+4,b);
+		  				var local = frame;
+		  				table = skip(table);
 
-	  				var local = frame;
-	  				table = skip(table);
+		  				//Code cours
+		  				a = table.indexOf('<td');
+		  				b = table.indexOf('</td>');
+		  				var frame = table.substring(a+4,b);
 
-	  				//Code cours
-	  				a = table.indexOf('<td');
-	  				b = table.indexOf('</td>');
-	  				var frame = table.substring(a+4,b);
+		  				var code = frame;
+		  				table = skip(table);
 
-	  				var code = frame;
-	  				table = skip(table);
+		  				var d = duration.split('h');
+		  				var da = startDate.split('h');
+		  				var endDate = (parseInt(d[0])+ parseInt(da[0])) + "h" + da[1]
 
-	  				var d = duration.split('h');
-	  				var da = startDate.split('h');
-	  				var endDate = (parseInt(d[0])+ parseInt(da[0])) + "h" + da[1]
+		  				var item = {
+		  					code: code,
+		  					date: date,
+		  					startDate : startDate,
+		  					endDate : endDate,
+		  					duration : duration,
+		  					professor : professor,
+		  					local :local,
+		  				};
+		  				list_schedule.push(item);
 
-	  				var item = {
-	  					code: code,
-	  					date: date,
-	  					startDate : startDate,
-	  					endDate : endDate,
-	  					duration : duration,
-	  					professor : professor,
-	  					local :local,
-	  				};
-	  				list_schedule.push(item);
+		  				b = doc.indexOf('</tr>');
+		  				doc = doc.substring(b+5);
+		  				a = doc.indexOf('<tr');
+		  			}
+		  			window.localStorage['schedule'] = list_schedule;
+		  			$scope.Schedule = list_schedule;
+		  		}, function errorCallback(data,status) {
+		    		console.log('fail schedule network load ' + status);
+		  		});
 
-	  				b = doc.indexOf('</tr>');
-	  				doc = doc.substring(b+5);
-	  				a = doc.indexOf('<tr');
-	  			}
+		    // this callback will be called asynchronously
+		    // when the response is available
+		  }, function errorCallback(data,status) {
+		    console.log('fail schedule network load ' + status);
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
 
-	  			$scope.Schedule = list_schedule;
-	  		}, function errorCallback(data,status) {
-	    		console.log(data);
-	  		});
-
-	    // this callback will be called asynchronously
-	    // when the response is available
-	  }, function errorCallback(data,status) {
-	    console.log(data);
-	    // called asynchronously if an error occurs
-	    // or server returns response with an error status.
-	  });
+	}
+	else{
+		$scope.schedule = schedList;
+	}
+	
 })
 
 $scope.isToday = function(item){
@@ -186,12 +194,12 @@ $scope.isToday = function(item){
 		month = "0" + month;
 	}
 	var year = $scope.datepickerObject.inputDate.getFullYear();
-	var selectedDate = day  + '/' + month + '/' + year;
+	var selectedDate = month  + '/' + day + '/' + year;
 	return (item.date == selectedDate);
 }
 
 var disabledDates = [];
-var weekDaysList = ["Sun", "Mon", "Tue", "Wed", "thu", "Fri", "Sat"];
+var weekDaysList = ["Su", "Mo", "Tu", "We", "th", "Fr", "Sa"];
 var monthList = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
 $scope.datepickerObject = {
