8ad9d4303cd1c03bbc37716a3ac6222478c8a94a
baplac <baptistelacasse@gmail.com>
2016-03-04 13:16:26 +0100

    Merge

--- UCLCampus/www/campus/events/js/events.js
+++ UCLCampus/www/campus/events/js/events.js
@@ -3,5 +3,13 @@ angular.module('ionicApp').controller("EventsController", function($scope, $stat
 	for(var i = 0; i<10; i++) {
 		$scope.eventList.push(events[i]);
 	}
+
+	$scope.categories = [];
+	for(var i = 0; i<$scope.eventList.length; i++) {
+		if($scope.categories.indexOf($scope.eventList[i].category)==-1 && $scope.eventList[i].category!="Autre") {
+			$scope.categories.push($scope.eventList[i].category);
+		}
+	}
+	$scope.categories.push("Autre");
 	
 })
